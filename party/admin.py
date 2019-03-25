91567d81477b509e7b42c5a65203df9efc2e34dc
Vishal Mishra <vishalm434@gmail.com>
2019-03-25 15:57:21 +0530

    Register model member on admin site

--- party/admin.py
+++ party/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import State, District
+from .models import State, District, Member
 
 admin.site.register(State)
 admin.site.register(District)
+admin.site.register(Member)
